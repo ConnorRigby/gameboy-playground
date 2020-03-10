18052defffb298d1033d820e376b4d80fbab79bf
Connor Rigby <konnorrigby@gmail.com>
2020-03-09 19:48:14 -0700

    refactor copy link to dma

--- src/main.asm
+++ src/main.asm
@@ -1,4 +1,5 @@
 INCLUDE "hardware.inc"
+; INCLUDE "debug.asm"
 
 SECTION "Header", ROM0[$100]
 EntryPoint: ; This is where execution begins
@@ -8,6 +9,9 @@ EntryPoint: ; This is where execution begins
 REPT $150 - $104
     db 0
 ENDR
+
+SECTION "vblankInt", ROM0[$40]
+    jp VBlankHandler
 
 SECTION "Game code", ROM0
 
@@ -19,7 +23,7 @@ Start:
     jr c, .waitVBlank
 
     ld a, %10000001
-    ldh [rKEY1], a 
+    ldh [rKEY1], a
     stop ; set cpu double speed mode
 
     xor a ; ld a, 0 ; We only need to reset a value with bit 7 reset, but 0 does the job
@@ -32,7 +36,7 @@ Start:
     ; set palette. first load is the second half of green and red.
     ; second load is the first half of green and blue
 
-    ; example: full red 
+    ; example: full red
     ; ld a, %00011111
     ; ld [rBCPD], a
 
@@ -53,22 +57,6 @@ Start:
     ld a, %01111100
     ld [rBCPD], a
 
-; .setupDMA
-;     ld de, FontTiles ; store FontTiles addr 
-;     ld hl, rHDMA1 ; source high
-;     ld [hl], d ; load high portion of FontTiles into rHDMA1
-;     ld hl, rHDMA2 ; source LOW
-;     ld [hl], e ; load low portion of FontTiles into rHDMA2
-
-;     ld de, $9000 ; dest addr
-;     ld hl, rHDMA3 ; dest high
-;     ld [hl], d ; load source high into rHDMA3
-;     ld hl, rHDMA4 ; dest low
-;     ld [hl], e ; load source low into rHDMA4
-
-;     ld hl, rHDMA5 ; start reg
-;     ld [hl], %11111111 ; this blindly coppies way too much data into $9000
-
 .setupDMA
     ld a, HIGH(FontTiles)
     ldh [rHDMA1], a
@@ -80,27 +68,6 @@ Start:
     ldh [rHDMA4], a
     ld a, $80 | 127 ; Starts a (127 + 1) * 16-byte copy
     ldh [rHDMA5], a
-
-;     ld hl, $9000 ; setup font copy. i'm not really sure where this value comes from.
-;     ld de, FontTiles
-;     ld bc, FontTilesEnd - FontTiles
-; .copyFont
-;     ld a, [de] ; Grab 1 byte from the source
-;     ld [hli], a ; Place it at the destination, incrementing hl
-;     inc de ; Move to next byte
-;     dec bc ; Decrement count
-;     ld a, b ; Check if count is 0, since `dec bc` doesn't update flags
-;     or c
-;     jr nz, .copyFont
-
-    ld hl, $9800 ; This will print the string at the top-left corner of the screen
-    ld de, HelloWorldStr
-.copyString
-    ld a, [de]
-    ld [hli], a
-    inc de
-    and a ; Check if the byte we just copied is zero
-    jr nz, .copyString ; Continue if it's not
 
     ; Init display registers
     ld a, %11100100
@@ -116,11 +83,26 @@ Start:
     ; Turn screen on, display background
     ld a, %10000001
     ld [rLCDC], a
+    ld a, %00000001
+    ld [rIE], a
+    ei ; enable interupts
 
     ; Lock up
 .lockup
     jr .lockup
 
+
+SECTION "vblank", ROM0
+VBlankHandler:
+    ld hl, $9800 ; This will print the string at the top-left corner of the screen
+    ld de, HelloWorldStr
+.copyString
+    ld a, [de]
+    ld [hli], a
+    inc de
+    and a ; Check if the byte we just copied is zero
+    jr nz, .copyString ; Continue if it's not
+    reti
 
 SECTION "Font", ROM0
 
